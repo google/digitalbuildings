dd38c97e577e644ea8a22aa540cb8e85a5c307a4
Charbel kaed <charbelk@google.com>
2020-05-19 14:09:25 -0400

    Update rdf_helper_test.py

--- tools/rdf_generator/rdfformat/tests/rdf_helper_test.py
+++ tools/rdf_generator/rdfformat/tests/rdf_helper_test.py
@@ -45,7 +45,7 @@ class RdfHelperLibTest(absltest.TestCase):
                   updated_graph)
     self.assertIn((clazz, rdflib.RDFS.label, rdflib.Literal('class_name')),
                   updated_graph)
-    self.assertIn((clazz, rdflib.RDFS.comment, rdflib.Literal('description')),
+    self.assertIn((clazz, rdflib.RDFS.comment, rdflib.Literal('description2')),
                   updated_graph)
 
   def testClassHelperNoDescription(self):
