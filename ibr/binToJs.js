cb73c3e380fcb9966fb6260bf12b6543d3cc6d09
Shuang Li <shuanglihtk@google.com>
2020-06-05 18:51:42 +0000

    decoded ibr data and visualize in UI using three.js

---
+++ ibr/binToJs.js
@@ -0,0 +1,55 @@
+/**
+ * @fileoverview This file takes a binary wire format ibr file, decode the information and store data in sessionStorage for visualization use
+ * @author Shuang Li
+ */
+
+function onFileLoad(event) {
+    var deserializedData = InternalBuildingRepresentation.read(new Pbf(event.target.result));
+    console.log(deserializedData);
+    var coordsIndexList = deserializedData.visualization[0].coordinates.coordinate_index;
+
+    // Decode Coordinates from data.coordinatesLookup.encodedData
+    var decoder = new TextDecoder('utf8');
+    var decodedCoordsString = atob(decoder.decode(deserializedData.coordinates_lookup.encoded_data));
+    console.log("decodedCoordsString: ");
+    console.log(typeof decodedCoordsString);
+
+    // Put coordinates in data structure
+    coords = decodedCoordsString.split(",");
+    var decodedCoordsList = [];
+    for (x in coords){
+        coordinate = coords[x].split(" ");
+        if (coordinate.length === 2){
+            decodedCoordsList.push(coordinate);
+        }
+    }
+
+    // Generate data to be visualized
+    var layerCoordinates = [];
+    console.log("decoded coordinates data: ");
+    for (x in coordsIndexList){
+        console.log(decodedCoordsList[coordsIndexList[x]]);
+        layerCoordinates.push(decodedCoordsList[coordsIndexList[x]])
+    }
+
+    sessionStorage.setItem('layerCoordinates', JSON.stringify(layerCoordinates));
+
+    window.location.href = "visualization.html";
+}
+
+function onChooseFile(event, onLoadFileHandler) {
+    if (typeof window.FileReader !== 'function')
+        throw ("The file API isn't supported on this browser.");
+    let input = event.target;
+    if (!input)
+        throw ("The browser does not properly implement the event object");
+    if (!input.files)
+        throw ("This browser does not support the `files` property of the file input.");
+    if (!input.files[0])
+        return undefined;
+    let file = input.files[0];
+    let fr = new FileReader();
+    fr.onload = onLoadFileHandler;
+    fr.readAsArrayBuffer(file);
+}
+
---
+++ ibr/visualization.js
@@ -0,0 +1,38 @@
+/**
+ * @fileoverview This file takes data from sessionStorage from binToJs.js and visualize them using three.js
+ * @author Shuang Li
+ */
+
+// Creating the scene
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 10000 );
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+// Importing uploaded data
+var material1 = new THREE.LineBasicMaterial( { color: 0x0000ff } );
+var points = [];
+layerCoordinates = JSON.parse(sessionStorage.getItem('layerCoordinates'));
+for (x in layerCoordinates){
+    points.push( new THREE.Vector3( layerCoordinates[x][0], layerCoordinates[x][1], 0 ) );
+    console.log(layerCoordinates[x]);
+}
+
+var geometry = new THREE.BufferGeometry().setFromPoints( points );
+var line = new THREE.LineLoop( geometry, material1 );
+scene.add( line );
+
+camera.position.set( 0, 0, 7000 );
+camera.lookAt( 0, 0, 0 );
+
+// Rendering the scene
+function animate() {
+	requestAnimationFrame( animate );
+	renderer.render( scene, camera );
+}
+animate();
+
+function back() {
+    window.location.href = "index.html";
+}
