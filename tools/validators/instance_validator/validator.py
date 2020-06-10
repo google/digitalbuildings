8df0c168be3496cd2f4d62f0e4106aac82d80488
Raymond Li <li.raymond@columbia.edu>
2020-06-10 17:10:36 -0400

    Parse all good Yaml successfully

--- tools/validators/instance_validator/validator.py
+++ tools/validators/instance_validator/validator.py
@@ -1,5 +1,16 @@
 from pykwalify.core import Core
 
-c = Core(source_file='test_data.yaml', schema_files=['schema.yaml'])
-res = c.validate(raise_exception=True)
-print(res)
+def validate_file(source, schema):
+    c = Core(source_file=source, schema_files=[schema])
+    res = c.validate(raise_exception=True)
+    return res
+
+# bad_1.yaml and bad_2.yaml are not immediately caught because of type: any
+# we can invalidate them later because their parsed outputs have None
+validate_file('bad_1.yaml', 'schema.yaml')
+validate_file('bad_2.yaml', 'schema.yaml')
+
+# bad_3 is caught because type and id require type: str
+validate_file('bad_3.yaml', 'schema.yaml')
+
+validate_file('bad_4.yaml', 'schema.yaml')
