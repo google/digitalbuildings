4a0642b5dce98f575561d81174eaafb88a9285a2
Shuang Li <shuanglihtk@google.com>
2020-06-12 18:54:28 +0000

    wrapped rendering function into npm package ibr-sdk

---
+++ ibr/ibr-sdk/src/renderer.js
@@ -0,0 +1,55 @@
+/**
+ * @fileoverview This file contains functions that render given ibr binary data to (array of) THREE objects.
+ * @author shuanglihtk@google.com (Shuang Li)
+ */
+
+function renderLayer(data) {
+
+    var deserializedData = InternalBuildingRepresentation.read(new Pbf(data));
+    var coordsIndexList = deserializedData.visualization[0].coordinates;
+    var coordsRangeBuffer = coordsIndexList.buffer.slice(coordsIndexList.byteOffset, coordsIndexList.buffer.byteLength);
+    var coordsRange = new Uint32Array(coordsRangeBuffer);
+    for (var i = 0; i < coordsRange.length; i++) {
+        coordsRange[i] = swap32(coordsRange[i]);
+    }
+
+
+    // Decode Coordinates from data.coordinatesLookup.encodedData
+    var decoder = new TextDecoder('utf8');
+    var decodedCoordsString = atob(decoder.decode(deserializedData.coordinates_lookup.encoded_data));
+
+    // Put coordinates in data structure
+    var coords = decodedCoordsString.split(",");
+    var decodedCoordsList = [];
+    for (const coord of coords) {
+        var coordinate = coord.split(" ");
+        if (coordinate.length === 2) {
+            decodedCoordsList.push(coordinate);
+        }
+    }
+
+    // Read multiple ranges from Visualization.coordinates array and store them in sessionStorage for visualization later
+    var layerCoordinates = [];
+    for (var i = 0; i < coordsRange.length; i += 2) {
+        layerCoordinates[i/2] = [];
+        for (var x = coordsRange[i]; x < coordsRange[i+1]; x++) {
+            layerCoordinates[i/2].push(decodedCoordsList[x]);
+        }
+    }
+
+    // Importing uploaded data
+    var material1 = new THREE.LineBasicMaterial( { color: 0x0000ff } );
+    var points = [], lines = [];
+    var geometry, line;
+    for (const layer of layerCoordinates) {
+        for (const coordinate of layer) {
+            points.push( new THREE.Vector3( coordinate[0], coordinate[1], 0 ) );
+        }
+        geometry = new THREE.BufferGeometry().setFromPoints( points );
+        line = new THREE.LineLoop( geometry, material1 );
+        lines.push( line );
+    }
+    return lines;
+}
+
+export { renderLayer };
