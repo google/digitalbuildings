557c3ca47cca37af1fdaeb15ff57181629418b9d
Shuang Li <shuanglihtk@google.com>
2020-07-01 21:18:04 +0000

    repackage functions for easier third-party use

--- ibr/ibr-sdk/build/ibr-sdk.js
+++ ibr/ibr-sdk/build/ibr-sdk.js
@@ -177,6 +177,164 @@ typeof define === 'function' && define.amd ? define(['exports'], factory) :
   }
 
   /**
+   * Create a side bar for layer and structure navigation.
+   * @param {binary} IBRRawData Raw data from IBR binary data file.
+   * @param {HTMLElement} parentElement Parent element to attach the sidebar to.
+   * @param {Object} scene Scene to draw the THREE.js visualization
+   */
+  function createSidebar(IBRRawData, parentElement, scene) {
+    const ibrData = InternalBuildingRepresentation.read(
+        new Pbf(IBRRawData));
+    // for datafiles that have top level name is ""
+    if (ibrData.name === '') {
+      ibrData.name = 'ibrData.name';
+    }
+    const li = document.createElement('li'); // list element container
+    parentElement.appendChild(li);
+    const rootSpan = createLabel('span', ibrData.name, li);
+    rootSpan.setAttribute('class', 'arrow');
+    li.appendChild(rootSpan);
+    const ul = document.createElement('ul');
+    ul.setAttribute('class', 'nested');
+    ul.setAttribute('id', ibrData.name);
+    li.appendChild(ul);
+    rootSpan.addEventListener('click', function() {
+      rootSpan.parentElement.querySelector('.nested').classList
+          .toggle('active');
+      rootSpan.classList.toggle('expanded-arrow');
+    });
+    // root structure index 0
+    extractSingleStructureData(ibrData, ibrData.name, scene, 0);
+  }
+
+  /**
+   * Generate Scene configured to display IBR data.
+   * @param {HTMLElement} parentElement parent HTML element that the
+   visualization will be append on.
+   * @return {Object} scene Scene generated to
+   */
+  function generateScene(parentElement) {
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(
+        75, window.innerWidth / window.innerHeight, 0.1, 10000 );
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    parentElement.appendChild( renderer.domElement );
+    const controls = new OrbitControls( camera, renderer.domElement );
+    controls.target.set( 0, 0.5, 0 );
+    controls.update();
+    controls.enablePan = false;
+    controls.enableDamping = true;
+    camera.position.set( 0, 0, 7000 );
+    camera.lookAt( 0, 0, 0 );
+    animate();
+
+    /**
+    * Renders the scene.
+    */
+    function animate() {
+      requestAnimationFrame( animate );
+      controls.update();
+      renderer.render( scene, camera );
+    }
+
+    return scene;
+  }
+
+  /**
+   * Create a new HTML Label Tag based on given name string and attach it to
+   * given parent tag.
+   * @param {string} tagName Type of tag to be created.
+   * @param {string} name InnerHTML of the Label tag.
+   * @param {tag} parentTag Tag to be attached to by the newly created label
+    tag.
+   * @param {string} [forId=undefined] ID to be set as the value of the newly
+    created label tag's for attribute. Needed if Label tag is created for a
+    checkbox.
+   * @return {tag} Newly created Label tag.
+   */
+  function createLabel(tagName, name, parentTag, forId = undefined) {
+    const label = document.createElement(tagName);
+    if (forId) {
+      label.setAttribute('for', forId);
+    }
+    label.innerHTML = name;
+    parentTag.appendChild(label);
+    return label;
+  }
+
+  /**
+   * Create checkboxes for given structure's layers and child structures.
+   * @param {Object} structureData Structure to be extracted and visualized.
+   * @param {String} curStructureId ID of the HTML div element to attach the
+   * new structure's data.
+   * @param {Object} scene object to attach the THREE.js objects
+   * @param {number} thisStructureIndex the index of the structure being
+   processed
+   * generated from structure layer data
+   */
+  function extractSingleStructureData(structureData, curStructureId, scene,
+      thisStructureIndex) {
+    const curStructure = IBRSDK.unpackStructure( structureData,
+        thisStructureIndex );
+    // Create checkbox for each layer
+    if (curStructure['layers'].size !== 0) {
+      for ( const [layerName, layer] of
+        Object.entries(curStructure['layers']) ) {
+        for ( const line of layer ) {
+          line.visible = false;
+          scene.add( line );
+        }
+        const checkBox = document.createElement('INPUT');
+        const div = document.createElement('DIV');
+        checkBox.setAttribute('type', 'checkbox');
+        checkBox.setAttribute('id', structureData.name + '_' + layerName);
+        div.appendChild(checkBox);
+        createLabel('label', layerName, div, structureData.name + '_' +
+        layerName);
+        document.getElementById(curStructureId).appendChild(div);
+        checkBox.addEventListener('change', function() {
+          if (checkBox.checked) {
+            for ( const line of layer ) {
+              line.visible = true;
+            }
+          } else {
+            for ( const line of layer ) {
+              line.visible = false;
+            }
+          }
+        });
+      }
+    }
+
+    // Create label for each child structure
+    for ( let structureIndex = 0; structureIndex <
+    curStructure['structures'].length; structureIndex++ ) {
+      const li = document.createElement('li');
+      document.getElementById(curStructureId).appendChild(li);
+      const label = createLabel('span',
+          curStructure['structures'][structureIndex].name, li);
+      label.setAttribute('class', 'arrow');
+      li.appendChild(label);
+      const ul = document.createElement('ul');
+      ul.setAttribute('class', 'nested');
+      ul.setAttribute('id', curStructure['structures'][structureIndex].name);
+      li.appendChild(ul);
+      label.addEventListener('click', function() {
+        label.parentElement.querySelector('.nested').classList.toggle('active');
+        label.classList.toggle('expanded-arrow');
+        if (label.getAttribute('value') == null) {
+          event.stopPropagation();
+          extractSingleStructureData(curStructure['structures'][structureIndex],
+              curStructure['structures'][structureIndex].name, scene,
+              structureIndex);
+          label.setAttribute('value', '0');
+        }
+      });
+    }
+  }
+
+  /**
    * Parse top level of decoded IBR Object into structures.
    * @param {binary} ibrRawData Raw data from IBR binary data file.
    * @param {number} structureIndex Index of current structure(floor).
