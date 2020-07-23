18404f310966a71ef8d6f4b370051ab496219dc2
Raymond Li <li.raymond@columbia.edu>
2020-07-23 13:37:36 -0400

    Activate pylint for instance validator

--- tools/validators/instance_validator/setup.py
+++ tools/validators/instance_validator/setup.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+"""Setup file for Instance Validator"""
+
 from setuptools import setup, find_packages
 
 setup(
