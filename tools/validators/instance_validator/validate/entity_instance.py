d43abb1e9eb1401e7975f2b4a6e745489b30ac1d
Raymond Li <li.raymond@columbia.edu>
2020-07-29 14:02:15 -0400

    Get ready for TDD

--- tools/validators/instance_validator/validate/entity_instance.py
+++ tools/validators/instance_validator/validate/entity_instance.py
@@ -32,6 +32,8 @@ class EntityInstance(findings_lib.Findings):
     self.entity = entity
     self.universe = universe
     self.required_keys = ('id', 'type')
+    self.translation_key = 'translation'
+    self.translation_compliant = 'COMPLIANT'
 
   def _ValidateType(self):
     """Uses information from the generated ontology universe to validate
@@ -57,6 +59,69 @@ class EntityInstance(findings_lib.Findings):
     if self.universe.GetEntityType(namespace, entity_type) is None:
       print('Invalid entity type:', entity_type)
       return False
+
+    return True
+
+  def _TranslationDeviceMapHasProperKeys(self, device_map):
+    """Determine whether the translation map of a device has proper keys.
+
+      Returns:
+        Returns boolean for whether a device translation follows one of the
+        valid provided formats.
+    """
+    units_long_form = set(['present_value', 'units'])
+    units_short_form = set(['present_value', 'unit_values'])
+    states = set(['present_value', 'states'])
+    valid_keysets = [units_short_form, units_long_form, states]
+
+    device_map_keys = device_map.keys()
+    for keyset in valid_keysets:
+      if device_map_keys.intersection(keyset) == set():
+        return True
+
+    return False
+
+  def _ValidateTranslation(self):
+    """Uses information from the generated ontology universe to validate
+    an entity's translation if it exists.
+
+    Returns:
+      Returns boolean for validity of entity translation, defaults to True if
+      translation is not specified.
+    """
+    if self.translation_key not in self.entity.keys():
+      return True
+
+    translation_body = self.entity[self.translation_key]
+
+    # check if translation is fully UDMI compliant
+    if translation_body == self.translation_compliant:
+      return True
+
+    # iterate through each translation device key and determine its form
+    for device_name in translation_body.keys():
+      # check if keys are UDMI compliant
+      if isinstance(translation_body[device_name], str):
+        continue
+
+      device_map = translation_body[device_name]
+      if not self._TranslationDeviceMapHasProperKeys(device_map):
+        print('Translation has improper keys:', device_name)
+        return False
+
+      # three remaining possibilities for translation format
+      if 'unit_values' in device_map.keys():
+        # check that each of the `unit_value` map keys are proper units
+        pass
+      elif 'states' in device_map.keys():
+        # check that the `states` map keys are proper states
+        pass
+      elif 'units' in device_map.keys():
+        # check that the unit map has keys named `keys`, `values`, and that `values` map keys are proper units
+        pass
+      else:
+        print('Translation has improper keys:', device_name)
+        return False
 
     return True
 
