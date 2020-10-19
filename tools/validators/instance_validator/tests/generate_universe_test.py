5eec5b6c65a1346b75f991647c52c6de08eee17b
Charbel kaed <charbelk@google.com>
2020-10-19 19:07:15 -0400

    Update generate_universe_test.py

--- tools/validators/instance_validator/tests/generate_universe_test.py
+++ tools/validators/instance_validator/tests/generate_universe_test.py
@@ -22,7 +22,8 @@ from validate import generate_universe
 from absl.testing import absltest
 from os import path
 
-_DEFAULT_ONTOLOGY_LOCATION = path.join('..', '..', '..', 'ontology', 'yaml', 'resources')
+_DEFAULT_ONTOLOGY_LOCATION = path.join('..', '..', '..'
+                                       , 'ontology', 'yaml', 'resources')
 _BAD_MODIFIED_ONTOLOGY = path.join('.', 'fake_resources', 'BAD', 'BAD_FORMAT')
 _NONEXISTENT_LOCATION = path.join('..', 'nonexistent')
 _EMPTY_FOLDER = path.join('.', 'fake_resources', 'BAD', 'BAD_EMPTY')
