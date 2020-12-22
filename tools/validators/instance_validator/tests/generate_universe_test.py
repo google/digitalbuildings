1c2364e8937a61b37625c31abc2d733c5f4340a2
Charbel kaed <charbelk@google.com>
2020-12-22 19:06:14 +0200

    Update generate_universe_test.py

--- tools/validators/instance_validator/tests/generate_universe_test.py
+++ tools/validators/instance_validator/tests/generate_universe_test.py
@@ -23,6 +23,8 @@ from absl.testing import absltest
 from os import path
 
 _TEST_DIR = path.dirname(path.realpath(__file__))
+_DEFAULT_ONTOLOGY_LOCATION = path.join(_TEST_DIR, '..', '..', '..', '..',
+                                       'ontology', 'yaml', 'resources')
 _BAD_MODIFIED_ONTOLOGY = path.join(_TEST_DIR,
                                    'fake_resources', 'BAD', 'BAD_FORMAT')
 _NONEXISTENT_LOCATION = path.join(_TEST_DIR, 'nonexistent')
@@ -39,6 +41,10 @@ class GenerateUniverseTest(absltest.TestCase):
       generate_universe.BuildUniverse(_BAD_MODIFIED_ONTOLOGY)
     self.assertIn('no longer valid', str(context.exception))
 
+  def testModifiedTypesFilepathWorks(self):
+    test_universe = generate_universe.BuildUniverse(_DEFAULT_ONTOLOGY_LOCATION)
+    self.assertTrue(test_universe)
+    
   def testModifiedTypesCatchesNonexistent(self):
     self.assertRaises(Exception,
                       generate_universe.BuildUniverse(_NONEXISTENT_LOCATION))
