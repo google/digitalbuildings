40bb4aea086e51669772c54aa9e4542848e16db0
Charbull <charbel.kaed@outlook.com>
2021-01-05 21:14:57 +0200

    update path

--- tools/validators/ontology_validator/yamlformat/tests/presubmit_validate_types_lib_test.py
+++ tools/validators/ontology_validator/yamlformat/tests/presubmit_validate_types_lib_test.py
@@ -38,7 +38,7 @@ _F = test_helpers_lib.Fields
 _F1 = test_helpers_lib.Field
 
 # Constant to point to test files.
-_TEST_DIR = path.dirname(path.realpath(__file__))
+_TEST_DIR = path.dirname(path.relpath(__file__))
 RESOURCE_PATH = path.join(_TEST_DIR, 'fake_resources')
 
 # Override this value to keep tests stable
