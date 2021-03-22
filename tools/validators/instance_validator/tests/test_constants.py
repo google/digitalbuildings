4ce8bf4c870f59b6281c1ee8457dab7e92461432
Charbel <charbelk@google.com>
2021-03-22 18:25:15 -0400

    Project import generated by Copybara.
    PiperOrigin-RevId: 364421689

---
+++ tools/validators/instance_validator/tests/test_constants.py
@@ -0,0 +1,11 @@
+"""Constants for the instance validator application tests."""
+from os import path
+
+from google3.third_party.digitalbuildings.tools.validators.instance_validator import constants
+
+ONTOLOGY_ROOT = constants.ONTOLOGY_ROOT
+TEST_ROOT = path.join(constants.APPLICATION_ROOT, 'tests')
+TEST_INSTANCES = path.join(TEST_ROOT, 'fake_instances')
+TEST_RESOURCES = path.join(TEST_ROOT, 'fake_resources')
+TEST_TELEMETRY = path.join(TEST_ROOT, 'fake_telemetry')
+
