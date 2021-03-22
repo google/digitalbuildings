cf34f86641d0e23531e1aacc74b27e1cfa233807
Charbel <charbelk@google.com>
2021-03-22 18:25:15 -0400

    Project import generated by Copybara.
    PiperOrigin-RevId: 364421689

---
+++ tools/validators/ontology_validator/yamlformat/tests/test_constants.py
@@ -0,0 +1,8 @@
+"""Constants for the ontology validator application tests."""
+from os import path
+
+from yamlformat import constants
+
+ONTOLOGY_ROOT = constants.ONTOLOGY_ROOT
+TEST_ROOT = path.join(constants.APPLICATION_ROOT, 'tests')
+TEST_RESOURCES = path.join(TEST_ROOT, 'fake_resources')
