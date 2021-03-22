4caf86ac19e6888a7f74fc2428d527aaf19b7135
Charbel <charbelk@google.com>
2021-03-22 18:25:15 -0400

    Project import generated by Copybara.
    PiperOrigin-RevId: 364421689

--- tools/validators/instance_validator/validate/generate_universe.py
+++ tools/validators/instance_validator/validate/generate_universe.py
@@ -11,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 """Generates the ontology universe for the instance validator."""
 
 from __future__ import absolute_import
@@ -19,16 +18,15 @@ from __future__ import print_function
 
 from os import path
 
+from instance_validator import constants
 from yamlformat.validator import external_file_lib
 from yamlformat.validator import namespace_validator
 from yamlformat.validator import presubmit_validate_types_lib
 
-_FILE_DIR = path.dirname(path.realpath(__file__))
-_DEFAULT_ONTOLOGY_LOCATION = path.join(_FILE_DIR, '..', '..', '..', '..',
-                                       'ontology', 'yaml', 'resources')
 
-
-def BuildUniverse(modified_types_filepath=None):
+def BuildUniverse(
+    modified_types_filepath: str = None
+) -> presubmit_validate_types_lib.ConfigUniverse:
   """Generates the ontology universe.
 
   Args:
@@ -40,23 +38,27 @@ def BuildUniverse(modified_types_filepath=None):
   if modified_types_filepath:
     modified_ontology_exists = path.exists(modified_types_filepath)
     if not modified_ontology_exists:
-      print('Specified filepath for modified ontology does not exist')
+      print('Specified filepath [{0}] modified ontology does not exist'.format(
+          modified_types_filepath))
       return None
 
     modified_types_filepath = path.expanduser(modified_types_filepath)
 
-    external_file_lib.Validate(filter_text=None,
-                               changed_directory=modified_types_filepath,
-                               original_directory=_DEFAULT_ONTOLOGY_LOCATION,
-                               interactive=False)
+    external_file_lib.Validate(
+        filter_text=None,
+        changed_directory=modified_types_filepath,
+        original_directory=constants.ONTOLOGY_ROOT,
+        interactive=False)
     yaml_files = external_file_lib.RecursiveDirWalk(modified_types_filepath)
   else:
-    default_ontology_exists = path.exists(_DEFAULT_ONTOLOGY_LOCATION)
+    default_ontology_exists = path.exists(constants.ONTOLOGY_ROOT)
     if not default_ontology_exists:
-      print('Specified filepath for default ontology does not exist')
+      print(
+          'Specified filepath [{0}] for default ontology does not exist'.format(
+              constants.ONTOLOGY_ROOT))
       return None
     # use default location for ontology files
-    yaml_files = external_file_lib.RecursiveDirWalk(_DEFAULT_ONTOLOGY_LOCATION)
+    yaml_files = external_file_lib.RecursiveDirWalk(constants.ONTOLOGY_ROOT)
 
   config = presubmit_validate_types_lib.SeparateConfigFiles(yaml_files)
   universe = presubmit_validate_types_lib.BuildUniverse(config)
