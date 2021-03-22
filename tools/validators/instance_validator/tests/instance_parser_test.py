1e21d59d593f4ecb3fd1d29d5358bc9dd5afcfac
Charbel <charbelk@google.com>
2021-03-22 18:25:15 -0400

    Project import generated by Copybara.
    PiperOrigin-RevId: 364421689

--- tools/validators/instance_validator/tests/instance_parser_test.py
+++ tools/validators/instance_validator/tests/instance_parser_test.py
@@ -11,18 +11,21 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Tests tools.validators.instance_validator.instance_parser"""
+"""Tests tools.validators.instance_validator.instance_parser."""
 
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from validate import instance_parser
-from absl.testing import absltest
 from os import path
 
-_TEST_DIR = path.dirname(path.realpath(__file__))
-_TESTCASE_PATH = path.join(_TEST_DIR, 'fake_instances')
+from absl.testing import absltest
+import strictyaml as syaml
+
+from instance_validator.tests import test_constants
+from validate import instance_parser
+
+_TESTCASE_PATH = test_constants.TEST_INSTANCES
 
 
 def _ParserHelper(testpaths):
@@ -38,6 +41,13 @@ def _Helper(testpaths):
 
 
 class ParserTest(absltest.TestCase):
+
+  def test_EnumToRegex(self):
+    expected = syaml.Regex('^(ADD) | (UPDATE)$')
+    actual = instance_parser.EnumToRegex(
+        instance_parser.EntityOperation,
+        [instance_parser.EntityOperation.DELETE])
+    self.assertEqual(str(expected), str(actual))
 
   def testInstanceValidatorDetectDuplicateKeys(self):
     with self.assertRaises(SystemExit):
@@ -81,56 +91,82 @@ class ParserTest(absltest.TestCase):
 
   def testInstanceValidatorDetectImproperTranslationCompliance(self):
     with self.assertRaises(SystemExit):
-      parser = _Helper(
+      parse = _Helper(
           [path.join(_TESTCASE_PATH, 'BAD', 'bad_translation_compliant.yaml')])
-      del parser
+      del parse
 
   def testInstanceValidatorDetectImproperTranslationKeys(self):
     with self.assertRaises(SystemExit):
-      parser = _Helper(
+      parse = _Helper(
           [path.join(_TESTCASE_PATH, 'BAD', 'bad_translation_keys.yaml')])
-      del parser
+      del parse
 
   def testInstanceValidatorDetectImproperUnitsKeys(self):
     with self.assertRaises(SystemExit):
-      parser = _Helper([
+      parse = _Helper([
           path.join(_TESTCASE_PATH, 'BAD', 'bad_translation_units_format.yaml')
       ])
-      del parser
+      del parse
 
   def testInstanceValidatorDetectDuplicateEntityKeys(self):
     with self.assertRaises(SystemExit):
-      parser = _Helper([
-          path.join(_TESTCASE_PATH, 'BAD', 'bad_duplicate_key.yaml')
-      ])
-      del parser
+      parse = _Helper(
+          [path.join(_TESTCASE_PATH, 'BAD', 'bad_duplicate_key.yaml')])
+      del parse
 
   def testInstanceValidatorDetectDuplicateMetadata(self):
     with self.assertRaises(SystemExit):
-      parser = _Helper([
-          path.join(_TESTCASE_PATH, 'BAD', 'bad_duplicate_metadata.yaml')
-      ])
-      del parser
+      parse = _Helper(
+          [path.join(_TESTCASE_PATH, 'BAD', 'bad_duplicate_metadata.yaml')])
+      del parse
+
+  def testInstanceValidatorRejectsOperationOnInitialize(self):
+    with self.assertRaises(SystemExit):
+      parse = _Helper(
+          [path.join(_TESTCASE_PATH, 'BAD', 'bad_entity_operation.yaml')])
+      del parse
+
+  def testInstanceValidatorRejectsMaskOnInitialize(self):
+    with self.assertRaises(SystemExit):
+      parse = _Helper(
+          [path.join(_TESTCASE_PATH, 'BAD', 'bad_entity_mask.yaml')])
+      del parse
+
+  def testInstanceValidatorRejectsMaskOnAdd(self):
+    with self.assertRaises(SystemExit):
+      parse = _Helper(
+          [path.join(_TESTCASE_PATH, 'BAD', 'bad_entity_add_mask.yaml')])
+      del parse
+
+  def testInstanceValidatorRejectsUpdateWithoutEtag(self):
+    with self.assertRaises(SystemExit):
+      parse = _Helper(
+          [path.join(_TESTCASE_PATH, 'BAD', 'bad_entity_etag.yaml')])
+      del parse
 
   def testInstanceValidatorReadsMetadata(self):
-    parser = _ParserHelper([
-        path.join(_TESTCASE_PATH, 'GOOD', 'good_with_metadata.yaml')
-    ])
+    parser = _ParserHelper(
+        [path.join(_TESTCASE_PATH, 'GOOD', 'good_with_metadata.yaml')])
+    self.assertLen(parser.GetEntities().keys(), 2)
+    self.assertEqual(parser.GetConfigMode(), instance_parser.ConfigMode.UPDATE)
+
+  def testInstanceValidatorReadsMetadataAtEnd(self):
+    parser = _ParserHelper(
+        [path.join(_TESTCASE_PATH, 'GOOD', 'good_with_metadata_at_end.yaml')])
     self.assertLen(parser.GetEntities().keys(), 2)
     self.assertEqual(parser.GetConfigMode(), instance_parser.ConfigMode.UPDATE)
 
   def testInstanceValidatorHandlesUpdateMode(self):
-    parser = _ParserHelper([
-        path.join(_TESTCASE_PATH, 'GOOD', 'good_update_with_metadata.yaml')
-    ])
-    self.assertLen(parser.GetEntities().keys(), 2)
+    parser = _ParserHelper(
+        [path.join(_TESTCASE_PATH, 'GOOD', 'good_update_with_metadata.yaml')])
+    self.assertLen(parser.GetEntities().keys(), 4)
 
   def testInstanceValidatorUsesDefaultMode(self):
-    parser = _ParserHelper([
-        path.join(_TESTCASE_PATH, 'GOOD', 'good_building_type.yaml')
-    ])
+    parser = _ParserHelper(
+        [path.join(_TESTCASE_PATH, 'GOOD', 'good_building_type.yaml')])
     self.assertEqual(parser.GetConfigMode(),
                      instance_parser.ConfigMode.Default())
+
 
 if __name__ == '__main__':
   absltest.main()
--- tools/validators/instance_validator/validate/handler.py
+++ tools/validators/instance_validator/validate/handler.py
@@ -15,30 +15,27 @@
 
 from __future__ import print_function
 
-import argparse
+from datetime import datetime
 import sys
+from typing import Callable, Dict, List, Optional
 
 from validate import entity_instance
 from validate import generate_universe
 from validate import instance_parser
 from validate import subscriber
 from validate import telemetry_validator
-from datetime import datetime
-from typing import Dict
+from yamlformat.validator import presubmit_validate_types_lib as pvt
 
 
-# Default timeout duration for telemetry validation test
-DEFAULT_TIMEOUT = 600
-
-
-def deserialize(yaml_files: str) -> Dict[str, entity_instance.EntityInstance]:
-  """Parses a yaml configuration file and deserialize it.
+def Deserialize(
+    yaml_files: List[str]) -> Dict[str, entity_instance.EntityInstance]:
+  """Parses a yaml configuration file and deserializes it.
 
   Args:
-    yaml_files: list of building configuration file.
+    yaml_files: list of building configuration files.
 
   Returns:
-    A map of entity name to Entity instance.
+    A map of entity name to EntityInstance.
   """
 
   print('Validating syntax please wait ...')
@@ -48,196 +45,190 @@ def deserialize(yaml_files: str) -> Dict[str, entity_instance.EntityInstance]:
     parser.AddFile(yaml_file)
   parser.Finalize()
 
+  default_entity_operation = instance_parser.EntityOperation.ADD
+  if parser.GetConfigMode() == instance_parser.ConfigMode.UPDATE:
+    default_entity_operation = instance_parser.EntityOperation.UPDATE
+
   entities = {}
   for entity_name, entity_yaml in parser.GetEntities().items():
-    entities[entity_name] = entity_instance.EntityInstance(entity_yaml)
+    entities[entity_name] = entity_instance.EntityInstance.FromYaml(
+        entity_yaml, default_entity_operation)
+  return entities, parser.GetConfigMode()
 
-  return entities
+
+def _ValidateConfig(
+    filenames: List[str],
+    universe: pvt.ConfigUniverse) -> List[entity_instance.EntityInstance]:
+  """Runs all config validaton checks."""
+  print('\nLoading config files...\n')
+  entities, config_mode = Deserialize(filenames)
+  print('\nStarting config validation...\n')
+  helper = EntityHelper(universe)
+  return helper.Validate(entities, config_mode)
 
 
-class ValidationHelper(object):
-  """A validation helper to coordinate the various steps of the validation."""
+def _ValidateTelemetry(subscription: str, service_account: str,
+                       entities: Dict[str, entity_instance.EntityInstance],
+                       report_filename: str, timeout: int) -> None:
+  """Runs all telemetry validation checks."""
+  helper = TelemetryHelper(subscription, service_account)
+  helper.Validate(entities, report_filename, timeout)
 
-  def __init__(self, args):
+
+def RunValidation(filenames: List[str],
+                  modified_types_filepath: str = None,
+                  subscription: str = None,
+                  service_account: str = None,
+                  report_filename: str = None,
+                  timeout: int = 60) -> None:
+  """Master runner for all validations."""
+  if bool(subscription) != bool(service_account):
+    print('Subscription and a service account file are '
+          'both needed for the telemetry validation!')
+    sys.exit(0)
+  print('\nStarting validator...\n')
+  print('\nStarting universe generation...\n')
+  universe = generate_universe.BuildUniverse(modified_types_filepath)
+  if not universe:
+    print('\nError generating universe')
+    sys.exit(0)
+  print('\nStarting config validation...\n')
+  entities = _ValidateConfig(filenames, universe)
+  if subscription:
+    print('\nStarting telemetry validation...\n')
+    _ValidateTelemetry(subscription, service_account, entities, report_filename,
+                       timeout)
+
+
+class TelemetryHelper(object):
+  """A validation helper to encapsulate telemetry validation.
+
+  Attributes:
+    subscription: resource string referencing the subscription to check
+    service_account_file: path to file with service account information
+  """
+
+  def __init__(self, subscription, service_account_file):
     super().__init__()
-    self._ParseArgs(args)
+    self.subscription = subscription
+    self.service_account_file = service_account_file
 
-  def Validate(self):
-    universe = self.GenerateUniverse(self.args.modified_types_filepath)
-    entity_instances = deserialize(self.filenames)
-    self.ValidateEntities(universe, entity_instances)
-    self.StartTelemetryValidation(self.subscription, self.service_account,
-                                  entity_instances)
-
-  def GenerateUniverse(self, modified_types_filepath=None):
-    """Generates the universe from the ontology.
+  def Validate(self, entities: Dict[str, entity_instance.EntityInstance],
+               report_filename: str, timeout: int) -> None:
+    """Validates telemetry payload received from the subscription.
 
     Args:
-     modified_types_filepath: the path to a modified ontology. If it is not set,
-       the universe is generated from the default path.
+      entities: EntityInstance dictionary keyed by entity name
+      report_filename: path to write results to
+      timeout: number of seconds to wait for telemetry
     """
-    # SYNTAX VALIDATION
-    print('\nValidator starting ...\n')
-    # prints for syntax errors and exits gracefully
 
-    print('Generating universe ...')
-    universe = generate_universe.BuildUniverse(modified_types_filepath)
-    if universe is None:
-      print('\nError generating universe')
-      sys.exit(0)
+    print('Connecting to pubsub subscription: ', self.subscription)
+    sub = subscriber.Subscriber(self.subscription, self.service_account_file)
+    validator = telemetry_validator.TelemetryValidator(
+        entities, timeout,
+        self.BuildTelemetryValidationCallback(report_filename))
+    validator.StartTimer()
+    sub.Listen(validator.ValidateMessage)
 
-    print('Universe generated successfully')
-    return universe
+  def BuildTelemetryValidationCallback(
+      self,
+      report_filename: Optional[str] = None
+  ) -> Callable[[telemetry_validator.TelemetryValidator], None]:
+    """Returns a callback to be called when a telemetry message is received.
 
-  def ValidateEntities(self, universe, entity_instances):
-    """Validates entity instances that are already deserialized.
+    Args:
+      report_filename: path to write results to
+    """
+
+    def TelemetryValidationCallback(
+        validator: telemetry_validator.TelemetryValidator) -> None:
+      """Callback when the telemetry validator finishes.
+
+      This could be called due to a timeout or because telemetry messages were
+      received and validated for every expected entity.
 
       Args:
-        universe: ConfigUniverse representing the ontology
-        entity_instances: a list of entity instances.
+        validator: the telemetry validator that triggered the callback.
+      """
+
+      print('Generating validation report ...')
+      current_time = datetime.now()
+      timestamp = current_time.strftime('%d-%b-%Y (%H:%M:%S)')
+      report = '\nReport Generated at: {0}\n'.format(timestamp)
+
+      if not validator.AllEntitiesValidated():
+        report += ('No telemetry message was received for the following '
+                   'entities:')
+        report += '\n'
+        for entity_name in validator.GetUnvalidatedEntityNames():
+          report += '  {0}\n'.format(entity_name)
+
+      report += '\nTelemetry validation errors:\n'
+      for error in validator.GetErrors():
+        report += error.GetPrintableMessage()
+
+      report += '\nTelemetry validation warnings:\n'
+      for warnings in validator.GetWarnings():
+        report += warnings.GetPrintableMessage()
+
+      if report_filename:
+        f = open(self.report_filename, 'w')
+        f.write(report)
+        f.close()
+      else:
+        print('\n')
+        print(report)
+      print('Report Generated')
+      sys.exit(0)
+
+    return TelemetryValidationCallback
+
+
+class EntityHelper(object):
+  """A validation helper to coordinate the various steps of the validation.
+
+  Attributes:
+    universe: ConfigUniverse to validate against
+  """
+
+  def __init__(self, universe: pvt.ConfigUniverse):
+    super().__init__()
+    self.universe = universe
+
+  def Validate(
+      self, entities: Dict[str, entity_instance.EntityInstance],
+      config_mode: instance_parser.ConfigMode
+  ) -> Dict[str, entity_instance.EntityInstance]:
+    """Validates entity instances that are already deserialized.
+
+    Args:
+      entities: a list of entity instances
+      config_mode: processing mode of the configuration
+
+    Returns:
+      A dictionary containing valid entities by name
+
+    Raises:
+      SyntaxError: If no building is found in the config
     """
     print('Validating entities ...')
     building_found = False
-    for entity_name, current_entity_instance in entity_instances.items():
-      if not current_entity_instance.IsValidEntityInstance(
-          universe, entity_instances):
-        print(entity_name, 'is not a valid instance')
-        sys.exit(0)
-      if current_entity_instance.type_name.lower() == 'building':
+    valid_entities = {}
+    validator = entity_instance.CombinationValidator(self.universe, config_mode,
+                                                     entities)
+    for entity_name, current_entity in entities.items():
+      if (current_entity.operation is not instance_parser.EntityOperation.DELETE
+          and current_entity.type_name.lower() == 'building'):
         building_found = True
+      if not validator.Validate(current_entity):
+        print(entity_name, 'is not a valid instance')
+        continue
+      valid_entities[entity_name] = entity_instance
 
     if not building_found:
-      print('Building Config must contain an entity with a building type')
+      print('Config must contain a non-deleted entity with a building type')
       raise SyntaxError('Building Config must contain an '
                         'entity with a building type')
     print('All entities validated')
-
-  def StartTelemetryValidation(self, subscription, service_account_file,
-                               entity_instances):
-    """Validates telemetry payload received from the subscription.
-
-     Args:
-       subscription: a pubsub subscription.
-       service_account_file: a GCP service account file.
-       entity_instances: EntityInstance dictionary
-    """
-    if self.pubsub_validation_set:
-      print('Connecting to pubsub subscription: ', subscription)
-      sub = subscriber.Subscriber(subscription, service_account_file)
-      validator = telemetry_validator.TelemetryValidator(
-          entity_instances, self.timeout, self.TelemetryValidationCallback)
-      validator.StartTimer()
-      sub.Listen(validator.ValidateMessage)
-
-  def TelemetryValidationCallback(self, validator):
-    """Callback when the telemetry validator finishes.
-
-    This could be called due to a timeout or because telemetry messages were
-    received and validated for every expected entity.
-
-    Args:
-      validator: the telemetry validator that triggered the callback.
-    """
-
-    print('Generating validation report ...')
-    current_time = datetime.now()
-    timestamp = current_time.strftime('%d-%b-%Y (%H:%M:%S)')
-    report = '\nReport Generated at: {0}\n'.format(timestamp)
-
-    if not validator.AllEntitiesValidated():
-      report += 'No telemetry message was received for the following entities:'
-      report += '\n'
-      for entity_name in validator.GetUnvalidatedEntityNames():
-        report += '  {0}\n'.format(entity_name)
-
-    report += '\nTelemetry validation errors:\n'
-    for error in validator.GetErrors():
-      report += error.GetPrintableMessage()
-
-    report += '\nTelemetry validation warnings:\n'
-    for warnings in validator.GetWarnings():
-      report += warnings.GetPrintableMessage()
-
-    if self.report_filename:
-      f = open(self.report_filename, 'w')
-      f.write(report)
-      f.close()
-    else:
-      print('\n')
-      print(report)
-    print('Report Generated')
-    sys.exit(0)
-
-  def _ParseArgs(self, args):
-    """Prepares the arguments for the user input."""
-    parser = argparse.ArgumentParser(
-        description='Validate a YAML building configuration file')
-
-    parser.add_argument(
-        '-i',
-        '--input',
-        action='append',
-        dest='filenames',
-        required=True,
-        help='Filepaths to YAML building configurations',
-        metavar='FILE')
-
-    parser.add_argument(
-        '-m',
-        '--modified-ontology-types',
-        dest='modified_types_filepath',
-        required=False,
-        help='Filepath to modified type filepaths',
-        metavar='MODIFIED_TYPE_FILEPATHS')
-
-    parser.add_argument(
-        '-s',
-        '--subscription',
-        dest='subscription',
-        required=False,
-        help='Pubsub subscription for telemetry to validate',
-        metavar='subscription')
-
-    parser.add_argument(
-        '-a',
-        '--service-account',
-        dest='service_account',
-        required=False,
-        help='Service account used to pull messages from the subscription',
-        metavar='service-account')
-
-    parser.add_argument(
-        '-t',
-        '--timeout',
-        dest='timeout',
-        required=False,
-        default=DEFAULT_TIMEOUT,
-        help='Timeout duration (in seconds) for telemetry validation test',
-        metavar='timeout')
-
-    parser.add_argument(
-        '-r',
-        '--report-filename',
-        dest='report_filename',
-        required=False,
-        default=None,
-        help='Filename for the validation report',
-        metavar='report-filename')
-
-    self.args = parser.parse_args(args)
-    self.filenames = self.args.filenames
-
-    self.subscription = self.args.subscription
-    self.service_account = self.args.service_account
-    self.timeout = int(self.args.timeout)
-    self.report_filename = self.args.report_filename
-
-    self.pubsub_validation_set = False
-    if self.subscription is not None and self.args.service_account is not None:
-      self.pubsub_validation_set = True
-    elif self.args.subscription is None and self.args.service_account is None:
-      self.pubsub_validation_set = False
-    else:
-      print('Subscription and a service account file are '
-            'both needed for the telemetry validation!')
-      sys.exit(0)
+    return valid_entities
