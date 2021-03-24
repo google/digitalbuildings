fe20f55ac3bac1de53a0f7f6cdce3b6edeb107ed
Charbull <charbel.kaed@outlook.com>
2021-03-23 18:48:58 -1000

    line

--- tools/validators/instance_validator/constants.py
+++ tools/validators/instance_validator/constants.py
@@ -2,7 +2,8 @@
 
 from os import path
 
-REPO_ROOT = path.join(path.dirname(path.realpath(__file__)), '..', '..', '..', '..')
+REPO_ROOT = path.join(path.dirname(path.realpath(__file__)),
+                      '..', '..', '..', '..')
 APPLICATION_ROOT = path.join(REPO_ROOT, 'tools', 'validators',
                              'instance_validator')
 ONTOLOGY_ROOT = path.join(REPO_ROOT, 'ontology', 'yaml', 'resources')
