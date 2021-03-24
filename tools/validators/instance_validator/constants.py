69713739bb320717ac49097182d6638fa60d4d22
Charbull <charbel.kaed@outlook.com>
2021-03-23 19:23:16 -1000

    fix number of hops

--- tools/validators/instance_validator/constants.py
+++ tools/validators/instance_validator/constants.py
@@ -3,7 +3,7 @@
 from os import path
 
 REPO_ROOT = path.join(path.dirname(path.realpath(__file__)),
-                      '..', '..', '..', '..')
+                      '..', '..', '..')
 APPLICATION_ROOT = path.join(REPO_ROOT, 'tools', 'validators',
                              'instance_validator')
 ONTOLOGY_ROOT = path.join(REPO_ROOT, 'ontology', 'yaml', 'resources')
