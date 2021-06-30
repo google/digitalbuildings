f344842e40907e450ae4417c75f17e824b7822a6
Charbel kaed <charbelk@google.com>
2021-06-30 16:17:50 -0400

    Update setup.py

--- tools/validators/instance_validator/setup.py
+++ tools/validators/instance_validator/setup.py
@@ -27,7 +27,8 @@ setup(
            'Charbel Kaed,'
            'Nigel Kilmer',
     packages=find_packages(),
-    install_requires=['protobuf>=3.14.0',
+    install_requires=['ruamel>=0.17.4', 
+                      'protobuf>=3.14.0',
                       'strictyaml>=1.1.1',
                       'google-cloud-pubsub',
                       'google-auth<2.0',
