083c5d5440bebd107f622379f0e7c1f532f91194
Adam Hemphill <ahemphill@users.noreply.github.com>
2021-08-26 10:15:44 -0700

    Editor settings

--- tools/scoring/utils/hash.py
+++ tools/scoring/utils/hash.py
@@ -1,4 +1,10 @@
 from dirhash import dirhash
 
 def hash(dirpath: str) -> tuple:
+    """Generates a hash for quickly comparing directories.
+
+    Returns:
+        A tuple containing the input string and the hash/checksum as a string of the hexadecimal digits
+    """
+
     return (dirpath, dirhash(dirpath, "sha1", ignore=[".*", ".*/"]))
