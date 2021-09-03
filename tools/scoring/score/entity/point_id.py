09baec10a394be2bbdbb161d497627a60faf1822
Adam Hemphill <ahemphill@users.noreply.github.com>
2021-09-03 14:31:20 -0700

    Copypasta

--- tools/scoring/score/entity/point_id.py
+++ tools/scoring/score/entity/point_id.py
@@ -1,3 +1,18 @@
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the License);
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an AS IS BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 # Entity Point Identification
 
 # For each entity we care that all of its points are identified successfully.  While applying a point to a reporting device should be trivial once the point is identified and named, it is more difficult for virtual devices, and therefore worth scoring.
