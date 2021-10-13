3f6a70b55f788a78fb6f36ebc6d4cdb4a62b83d6
travis <trav.cwelch@gmail.com>
2021-10-13 14:55:01 -0700

    add application constants

---
+++ tools/explorer/lib/constants.py
@@ -0,0 +1,8 @@
+"""Constants for the ontology explorer application."""
+
+from os import path
+
+JOINS = path.join('..', '..', '..')
+REPO_ROOT = path.join(path.dirname(path.realpath(__file__)), JOINS)
+APPLICATION_ROOT = path.join(REPO_ROOT, 'tools', 'explorer', 'lib')
+ONTOLOGY_ROOT = path.join(REPO_ROOT, 'ontology', 'yaml', 'resources')
