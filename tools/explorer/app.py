ab791f858af0dfda94474e566d6928a967bef341
travis <trav.cwelch@gmail.com>
2021-10-13 16:05:04 -0700

    add licensing

--- tools/explorer/app.py
+++ tools/explorer/app.py
@@ -1,3 +1,17 @@
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the License);
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an AS IS BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """Main module for DBO explorer."""
 from lib import explorer
 from lib import model
@@ -12,7 +26,7 @@ flags.DEFINE_string('alt', None, 'Path to an alternate ontology')
 flags.DEFINE_boolean('debug', False, 'Produces debugging output')
 
 def main(argv):
-  """Main method for dbo.tools.explorer which runs explorer as an abseil app"""
+  """Main method for DBO explorer."""
   if FLAGS.debug:
     print('non-flag arguments:', argv)
   figlet_out = pyfiglet.figlet_format('DBO Explorer', font='digital')
--- tools/explorer/lib/explorer.py
+++ tools/explorer/lib/explorer.py
@@ -1,3 +1,17 @@
+# Copyright 202i Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the License);
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an AS IS BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """Main module for DBO explorer."""
 from yamlformat.validator import external_file_lib
 from yamlformat.validator import presubmit_validate_types_lib
