09f65b8dd38674401ec1183650b3fad9914e12cd
Travis Welch <trav.cwelch@gmail.com>
2021-10-19 12:28:36 -0700

    Add main module to explorer (#374)
    * add application constants
    
    * add pyfiglet to dependencies
    
    * add explorer build module
    
    * add main module
    
    * fix linter errors
    
    * add licensing
    
    * minor changes
    
    * minor app.py changes
    
    * add license to constants.py
    
    * minor changes
    
    * add argparse to dependency list
    
    * add argparse to main module
    
    * minor syntax changes and fix linter errors
    
    * add testing for arg_parser.py
    
    * add readme
    
    * Update readme
    
    * Add punctuation to doc strings
    
    Co-authored-by: Charbel kaed <charbelk@google.com>

--- tools/explorer/lib/tests/arg_parser_test.py
+++ tools/explorer/lib/tests/arg_parser_test.py
@@ -1,4 +1,4 @@
-# Copyright 2020 Google LLC
+# Copyright 2021 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the License);
 # you may not use this file except in compliance with the License.
@@ -11,18 +11,21 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Setup file for the ontology explorer."""
 
-from setuptools import find_packages
-from setuptools import setup
+"""Testing module for arg_parser.py."""
+from absl.testing import absltest
+import argparse
 
-setup(
-    name='ontology-explorer',
-    version='0.0.1',
-    url='https://github.com/google/digitalbuildings',
-    license='Apache License',
-    author='Travis Welch',
-    packages=find_packages(),
-    install_requires=['absl-py','pyfiglet','argparse'],
-    python_requires='>=3.7',
-)
+from lib import arg_parser
+
+class ArgParserTest(absltest.TestCase):
+
+  def setUp(self):
+    pass
+
+  def testParseArgs(self):
+    output_parser = arg_parser.ParseArgs()
+    self.assertEqual(type(output_parser), argparse.ArgumentParser)
+
+if __name__ == '__main__':
+  absltest.main()
