d610d82e5636762fa180280a2b858a8544a6985b
Travis Welch <trav.cwelch@gmail.com>
2021-10-20 14:17:44 -0700

    add arg_parser module to ontology_validator/ with tests (#379)
    * add arg_parser module to ontology_validator/ with tests
    
    * fix typo
    
    * fix more typos
    
    * minor fixes and readme update
    
    * minor changes
    
    * update readme
    
    Co-authored-by: Charbel kaed <charbelk@google.com>

--- tools/validators/ontology_validator/yamlformat/tests/arg_parser_test.py
+++ tools/validators/ontology_validator/yamlformat/tests/arg_parser_test.py
@@ -1,4 +1,4 @@
-# Copyright 2020 Google LLC
+# Copyright 2021 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the License);
 # you may not use this file except in compliance with the License.
@@ -12,18 +12,20 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from setuptools import setup, find_packages
-# with open("README.md", "r") as fh:
-#   long_description = fh.read()
-setup(
-    name='ontology-yaml-validator',
-    version='0.0.1',
-    url='https://github.com/google/digitalbuildings',
-    license='Apache License',
-    author='',
-    author_email='',
-    description='',
-    packages=find_packages(),
-    install_requires=['absl-py', 'pyglib', 'pyyaml>=5.3', 'argparse'],
-    python_requires='>=3.6',
-)
+"""Testing module for arg_parser.py."""
+from absl.testing import absltest
+import argparse
+
+from yamlformat import arg_parser
+
+class ArgParserTest(absltest.TestCase):
+
+  def setUp(self):
+    pass
+
+  def testParseArgs(self):
+    output_parser = arg_parser.ParseArgs()
+    self.assertEqual(type(output_parser), argparse.ArgumentParser)
+
+if __name__ == '__main__':
+  absltest.main()
