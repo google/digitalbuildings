e5565e5f83a0dfe487a2028f1aa59aed8c8e58b6
Adam Hemphill <ahemphill@users.noreply.github.com>
2022-03-29 12:37:43 -0700

    WIP

--- tools/scoring/tests/dimensions/entity_connection_identification_test.py
+++ tools/scoring/tests/dimensions/entity_connection_identification_test.py
@@ -18,35 +18,176 @@
 from absl.testing import absltest
 
 from score.dimensions.entity_connection_identification import EntityConnectionIdentification
-from score.constants import FileTypes
+from score.constants import FileTypes, DimensionCategories
+
+from tests.helper import TestHelper
 
 PROPOSED, SOLUTION = FileTypes
+SIMPLE, COMPLEX = DimensionCategories
 
 
 class EntityConnectionIdentificationTest(absltest.TestCase):
   def setUp(self):
     super().setUp()
-    # TODO: add real data (append cases to existing tests)
-    deserialized_files = {PROPOSED: {}, SOLUTION: {}}
-    self.entity_connection_identification = EntityConnectionIdentification(
-        deserialized_files=deserialized_files).evaluate()
+    featureful_file_path = (
+        'tests/samples/proposed/entity_connection_identification.yaml')
+    self.highest_score_argument = TestHelper.prepare_dimension_argument(
+        dimension_type=COMPLEX,
+        proposed_path=featureful_file_path,
+        solution_path=featureful_file_path)
 
-  def testDirectlyAssignedAttributes(self):
-    self.assertEqual(
-        self.entity_connection_identification.correct_total_override, 0)
-    self.assertEqual(
-        self.entity_connection_identification.correct_ceiling_override, 0)
-    self.assertEqual(
-        self.entity_connection_identification.incorrect_total_override, 0)
+    empty_file_path = 'tests/samples/empty.yaml'
+    self.none_score_argument = TestHelper.prepare_dimension_argument(
+        dimension_type=COMPLEX,
+        proposed_path=empty_file_path,
+        solution_path=empty_file_path)
 
-  def testInheritedCalculatedAttributes(self):
-    self.assertEqual(self.entity_connection_identification.correct_total(), 0)
-    self.assertEqual(self.entity_connection_identification.correct_ceiling(), 0)
-    self.assertEqual(self.entity_connection_identification.incorrect_total(), 0)
+    reporting_entity_file_path = (
+        'tests/samples/proposed/entity_connection_identification.yaml')
+    self.middling_score_argument = TestHelper.prepare_dimension_argument(
+        dimension_type=COMPLEX,
+        proposed_path=reporting_entity_file_path,
+        solution_path=featureful_file_path)
 
-  def testInheritedResultProperties(self):
-    # This is `None` by virtue of the ceiling being falsy.
-    self.assertEqual(self.entity_connection_identification.result_all, None)
+  def testEvaluate_ScoreNone(self):
+    """When ceiling==0, the resulting score is None. The ceiling is 0
+    because the solution does not contain any entities."""
+    none_score_expected = EntityConnectionIdentification(
+        deserialized_files=self.none_score_argument).evaluate()
+
+    # Directly assigned attributes
+
+    # Reporting and virtual entities are not evaluated separately,
+    # thus these values are not overridden by the child class
+    self.assertEqual(none_score_expected.correct_reporting, None)
+    self.assertEqual(none_score_expected.correct_ceiling_reporting, None)
+    self.assertEqual(none_score_expected.incorrect_reporting, None)
+
+    self.assertEqual(none_score_expected.correct_virtual, None)
+    self.assertEqual(none_score_expected.correct_ceiling_virtual, None)
+    self.assertEqual(none_score_expected.incorrect_virtual, None)
+
+    # These are set to allow for an "all" score to be calculated
+    # in the absence of "reporting" and "virtual" components
+    self.assertEqual(none_score_expected.correct_total_override, 'Foo')
+    self.assertEqual(none_score_expected.correct_ceiling_override, 'Bar')
+    self.assertEqual(none_score_expected.incorrect_total_override, 'Baz')
+
+    # Inherited calculated attributes
+    self.assertEqual(none_score_expected.correct_total(), 0)
+    self.assertEqual(none_score_expected.correct_ceiling(), 0)
+    self.assertEqual(none_score_expected.incorrect_total(), 0)
+
+    # Inherited result properties. These are `None` by virtue of the ceiling
+    # being falsy: that is to say, there was nothing to score against.
+    self.assertEqual(none_score_expected.result_all, None)
+    self.assertEqual(none_score_expected.result_reporting, None)
+    self.assertEqual(none_score_expected.result_virtual, None)
+
+  def testEvaluate_ScoreHighestPossible(self):
+    """When correct==ceiling, the resulting score is 1.0. All entities
+    correspond because the proposal is the same as the solution."""
+    highest_score_expected = EntityConnectionIdentification(
+        deserialized_files=self.highest_score_argument).evaluate()
+
+    # Directly assigned attributes
+
+    # Reporting and virtual entities are not evaluated separately,
+    # thus these values are not overridden by the child class
+    self.assertEqual(highest_score_expected.correct_reporting, None)
+    self.assertEqual(highest_score_expected.correct_ceiling_reporting, None)
+    self.assertEqual(highest_score_expected.incorrect_reporting, None)
+
+    self.assertEqual(highest_score_expected.correct_virtual, None)
+    self.assertEqual(highest_score_expected.correct_ceiling_virtual, None)
+    self.assertEqual(highest_score_expected.incorrect_virtual, None)
+
+    # These are set to allow for an "all" score to be calculated
+    # in the absence of "reporting" and "virtual" components
+    self.assertEqual(highest_score_expected.correct_total_override, 'Foo')
+    self.assertEqual(highest_score_expected.correct_ceiling_override, 'Bar')
+    self.assertEqual(highest_score_expected.incorrect_total_override, 'Baz')
+
+    # Inherited calculated attributes
+    self.assertEqual(highest_score_expected.correct_total(), 2)
+    self.assertEqual(highest_score_expected.correct_ceiling(), 2)
+    self.assertEqual(highest_score_expected.incorrect_total(), 0)
+
+    # Inherited result properties
+    self.assertEqual(highest_score_expected.result_all, 1.0)
+    self.assertEqual(highest_score_expected.result_reporting, 1.0)
+    self.assertEqual(highest_score_expected.result_virtual, 1.0)
+
+  def testEvaluate_ScoreLowestPossible(self):
+    """When correct==0, the resulting score is -1.0. No entities
+    correspond because the proposal does not contain any entities."""
+    lowest_score_argument = {
+        PROPOSED:
+        self.none_score_argument[PROPOSED],  # Empty, i.e. nothing correct
+        SOLUTION: self.highest_score_argument[SOLUTION]
+    }
+    lowest_score_expected = EntityConnectionIdentification(
+        deserialized_files=lowest_score_argument).evaluate()
+
+    # Directly assigned attributes
+
+    # Reporting and virtual entities are not evaluated separately,
+    # thus these values are not overridden by the child class
+    self.assertEqual(lowest_score_expected.correct_reporting, None)
+    self.assertEqual(lowest_score_expected.correct_ceiling_reporting, None)
+    self.assertEqual(lowest_score_expected.incorrect_reporting, None)
+
+    self.assertEqual(lowest_score_expected.correct_virtual, None)
+    self.assertEqual(lowest_score_expected.correct_ceiling_virtual, None)
+    self.assertEqual(lowest_score_expected.incorrect_virtual, None)
+
+    # These are set to allow for an "all" score to be calculated
+    # in the absence of "reporting" and "virtual" components
+    self.assertEqual(lowest_score_expected.correct_total_override, 'Foo')
+    self.assertEqual(lowest_score_expected.correct_ceiling_override, 'Bar')
+    self.assertEqual(lowest_score_expected.incorrect_total_override, 'Baz')
+
+    # Inherited calculated attributes
+    self.assertEqual(lowest_score_expected.correct_total(), 0)
+    self.assertEqual(lowest_score_expected.correct_ceiling(), 2)
+    self.assertEqual(lowest_score_expected.incorrect_total(), 2)
+
+    # Inherited result properties
+    self.assertEqual(lowest_score_expected.result_all, -1.0)
+    self.assertEqual(lowest_score_expected.result_reporting, None)
+    self.assertEqual(lowest_score_expected.result_virtual, None)
+
+  def testEvaluate_ScoreMiddling(self):
+    """When correct is half of the ceiling, the resulting score is 0.0. In this
+    case, reporting entities scored 1.0 and virtual entities scored -1.0; the
+    result for all entities is 0.0."""
+    middling_score_expected = EntityConnectionIdentification(
+        deserialized_files=self.middling_score_argument).evaluate()
+
+    # Directly assigned attributes
+    self.assertEqual(middling_score_expected.correct_reporting, None)
+    self.assertEqual(middling_score_expected.correct_ceiling_reporting, None)
+    self.assertEqual(middling_score_expected.incorrect_reporting, None)
+
+    self.assertEqual(middling_score_expected.correct_virtual, None)
+    self.assertEqual(middling_score_expected.correct_ceiling_virtual, None)
+    self.assertEqual(middling_score_expected.incorrect_virtual, None)
+
+    # These are set to allow for an "all" score to be calculated
+    # in the absence of "reporting" and "virtual" components
+    self.assertEqual(middling_score_expected.correct_total_override, 'Foo')
+    self.assertEqual(middling_score_expected.correct_ceiling_override, 'Bar')
+    self.assertEqual(middling_score_expected.incorrect_total_override, 'Baz')
+
+    # Inherited calculated attributes
+    self.assertEqual(middling_score_expected.correct_total(), 1)
+    self.assertEqual(middling_score_expected.correct_ceiling(), 2)
+    self.assertEqual(middling_score_expected.incorrect_total(), 1)
+
+    # Inherited result properties
+    self.assertEqual(middling_score_expected.result_all, 0.0)
+    self.assertEqual(middling_score_expected.result_reporting, None)
+    self.assertEqual(middling_score_expected.result_virtual, None)
 
 
 if __name__ == '__main__':
