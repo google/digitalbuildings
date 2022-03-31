7c1c952dc9a253ee5abf190bb98c097c89757471
Adam Hemphill <ahemphill@users.noreply.github.com>
2022-03-31 13:32:29 -0700

    Entity point identification test (#518)
    * Entity type identification
    
    * Separate evaluation of reporting and virtual entities
    
    * Prefix protected methods
    
    * Clarify docstring
    
    * Add perfect score test
    
    * Update entity_identification_test.py
    
    * Update entity_identification_test.py
    
    * Entity identification test
    
    * Update entity_identification_test.py
    
    * Update .gitignore
    
    * Update entity_identification_test.py
    
    * Update entity_identification_test.py
    
    * Update entity_type_identification_test.py
    
    * Add comments
    
    * Update path variable names
    
    * Update entity_identification_test.py
    
    * Fix typo
    
    * Publicize methods
    
    * Create deserialized file for specific purpose rather than cloning and modifying
    
    * Don't mess with existing fixtures
    
    * More fixes
    
    * Rename types_.py
    
    * Add note
    
    * Update entity_identification_test.py
    
    * Publicize aggregate_results()
    
    * Rename and add context to docstrings
    
    * Remove entity_type_identification.py
    
    * Add entity type identification
    
    * Make prepare_dimension_argument modular
    
    * Backport prepare_dimension_argument
    
    * D'oh
    
    * Rename
    
    * Add reporting entity as FCU-1 will be skipped
    
    * Set expectations
    
    * Fix reporting summation
    
    * Fix incorrect incorrects
    
    * Fix middling assertions and proposed file
    
    * Update entity_type_identification.py
    
    * Don't use relative path
    
    * Move sort/filter functionality
    
    * Pylint fix?
    
    * Remove ID fields from config files
    
    * Update entity_type_identification_virtual.yaml
    
    * Update comment
    
    * Clarify incorrect; add todo
    
    * Move sort/filter functionality
    
    * Update docstrings
    
    * Update docstrings; fix incorrect calculation
    
    * Add comprehensive test cases
    
    * Wrong type of type
    
    * Shorten line
    
    * rm todo
    
    * Passively test intermingling of new config format
    
    * Update entity_type_identification_test.py

--- tools/scoring/tests/dimensions/entity_type_identification_test.py
+++ tools/scoring/tests/dimensions/entity_type_identification_test.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Test for configuration file scoring tool
-"entity type identification" dimension (entity_type_identification.py)."""
+"entity type identification" dimension."""
 
 from absl.testing import absltest
 
@@ -31,20 +31,20 @@ class EntityTypeIdentificationTest(absltest.TestCase):
     featureful_file_path = (
         'tests/samples/proposed/entity_type_identification_virtual.yaml')
     self.highest_score_argument = TestHelper.prepare_dimension_argument(
-        entity_type=COMPLEX,
+        dimension_type=COMPLEX,
         proposed_path=featureful_file_path,
         solution_path=featureful_file_path)
 
     empty_file_path = 'tests/samples/empty.yaml'
     self.none_score_argument = TestHelper.prepare_dimension_argument(
-        entity_type=COMPLEX,
+        dimension_type=COMPLEX,
         proposed_path=empty_file_path,
         solution_path=empty_file_path)
 
     reporting_entity_file_path = (
         'tests/samples/proposed/entity_type_identification_reporting.yaml')
     self.middling_score_argument = TestHelper.prepare_dimension_argument(
-        entity_type=COMPLEX,
+        dimension_type=COMPLEX,
         proposed_path=reporting_entity_file_path,
         solution_path=featureful_file_path)
 
