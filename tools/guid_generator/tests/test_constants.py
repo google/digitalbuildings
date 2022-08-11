77c70dc6edb49b59341d0f64de8f1db343097fac
trav3711 <twelch@dbengineering.net>
2022-08-11 21:14:20 +0000

    Modify test constants imports

--- tools/guid_generator/tests/test_constants.py
+++ tools/guid_generator/tests/test_constants.py
@@ -1,7 +1,7 @@
 """Constants for the guid generator application tests."""
 from os import path
 
-from guid_generator import constants
+import constants
 
 TEST_ROOT = path.join(constants.APPLICATION_ROOT, 'tests')
 TEST_INSTANCES = path.join(TEST_ROOT, 'fake_instances')
