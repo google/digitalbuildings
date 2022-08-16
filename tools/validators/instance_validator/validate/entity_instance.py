42716f5859b1c746d232d7892bc2005832f68b5a
sypks <shaneypkspencer@gmail.com>
2022-08-16 21:23:23 +0000

    to send for review

--- tools/validators/instance_validator/validate/entity_instance.py
+++ tools/validators/instance_validator/validate/entity_instance.py
@@ -32,7 +32,9 @@ from yamlformat.validator import presubmit_validate_types_lib as pvt
 _CONFIG_UPDATE = parse.ConfigMode.UPDATE
 _CONFIG_INIT = parse.ConfigMode.INITIALIZE
 _CONFIG_EXPORT = parse.ConfigMode.EXPORT
-_UDMI_PRESENT_VALUE_REGEX = r'^[a-z][a-z0-9]*(_[a-z0-9]+)*$'
+# udmi present value: points.$name.present_value 
+# where name follows: [a-z][a-z0-9]*(_[a-z0-9]+)*
+_UDMI_PRESENT_VALUE_REGEX = r'^(points.)[a-z][a-z0-9]*(_[a-z0-9]+)*(.present_value)$'
 _UDMI_PRESENT_VALUE_PATTERN = re.compile(_UDMI_PRESENT_VALUE_REGEX)
 
 
