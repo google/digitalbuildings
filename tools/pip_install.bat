b02fa5f651247968c2b5a00c4f9e47213e435a03
trav3711 <twelch@dbengineering.net>
2022-11-10 17:09:39 +0000

    Use python instead of python3

--- tools/pip_install.bat
+++ tools/pip_install.bat
@@ -4,28 +4,28 @@ ECHO "Starting setup..."
 :ontology_validator_setup
 ECHO "Starting ontology validator setup"
 CD validators\ontology_validator
-START python3 -m pip install .
+START python -m pip install .
 ECHO "Finished ontology validator setup"
 CD ..\..\
 
 :instance_validator_setup
 ECHO "Starting instance validator setup"
 CD validators\instance_validator
-START python3 -m pip install .
+START python -m pip install .
 ECHO "Finished instance validator setup"
 CD ..\..
 
 :guid_generator_setup
 ECHO "Starting GUID generator setup"
 CD guid_generator
-START python3 -m pip install .
+START python -m pip install .
 ECHO "Finished GUID generator setup"
 CD ..
 
 :abel_setup
 ECHO "Starting ABEL setup"
 CD abel
-START python3 -m pip install .
+START python -m pip install .
 ECHO "Finished ABEL setup"
 CD ..
 
