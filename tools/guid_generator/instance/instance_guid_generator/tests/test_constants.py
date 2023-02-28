beef82ff3e26ece625196ec34a16c436952c582f
travis <twelch@dbengineering.net>
2023-02-28 15:45:48 -0800

    update constants

--- tools/guid_generator/instance/instance_guid_generator/tests/test_constants.py
+++ tools/guid_generator/instance/instance_guid_generator/tests/test_constants.py
@@ -1,7 +1,7 @@
 """Constants for the guid generator application tests."""
 from os import path
 
-from types_guid_generator import constants
+from instance_guid_generator import constants
 
 TEST_ROOT = path.join(constants.APPLICATION_ROOT, 'tests')
 TEST_INSTANCES = path.join(TEST_ROOT, 'fake_instances')
