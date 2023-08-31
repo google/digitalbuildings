ea6f5e907655881ed78427b23bc39802454c611a
travis <twelch@dbengineering.net>
2023-08-31 08:55:23 -0700

    fix docker_setup.sh

--- tools/scripts/docker_setup.sh
+++ tools/scripts/docker_setup.sh
@@ -6,6 +6,6 @@ source /env/bin/activate
 cd /source/tools
 ./pip_install.sh
 pip install -r /source/requirements.txt
-mkdir /source/tools.app_data
+mkdir /source/tools/.app_data
 mkdir /source/tools/.app_data/uploads
 mkdir /source/tools/.app_data/reports
