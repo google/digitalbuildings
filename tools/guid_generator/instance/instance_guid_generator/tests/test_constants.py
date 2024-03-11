929e3ad693b367cb43a9c76baad87d71fbb8c5c3
briancjohns <brian.johnson@otipro.com>
2024-03-11 10:44:24 -0400

    Added UPS battery remaining time

--- tools/guid_generator/instance/instance_guid_generator/tests/test_constants.py
+++
@@ -1,7 +0,0 @@
-"""Constants for the guid generator application tests."""
-from os import path
-
-from instance_guid_generator import constants
-
-TEST_ROOT = path.join(constants.APPLICATION_ROOT, 'tests')
-TEST_INSTANCES = path.join(TEST_ROOT, 'fake_instances')
--- tools/validators/instance_validator/validate/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Contains code to generate the ontology universe and work with
-EntityInstance variables"""
-
-from os import path
-import sys
-
-sys.path.append(path.abspath(path.join('..', 'ontology_validator')))
